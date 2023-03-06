4c5163c15c1eba843f7902a7247fdce858dde7a2
Noa <noa.trachez@epitech.eu>
2023-03-06 11:42:38 +0100

    [+] SEED jobs

--- job_dsl.groovy
+++ job_dsl.groovy
@@ -7,11 +7,35 @@ freeStyleJob('Tools/clone-repository') {
     parameters {
         stringParam('REPOSITORY_URL', '', 'URL of the repository to clone.')
     }
-    steps {
-        shell('git clone ${REPOSITORY_URL}')
-    }
     wrappers {
         preBuildCleanup()
     }
+    steps {
+        shell('git clone ${REPOSITORY_URL}')
+    }
 }
 
+freeStyleJob('Tools/SEED') {
+    parameters {
+        stringParam('GITHUB_NAME', '', 'GitHub repository owner/repo_name (e.g.: "EpitechIT31000/chocolatine")')
+        stringParam('DISPLAY_NAME', '', 'Display name for the job')
+    }
+    scm {
+        github('$GITHUB_NAME')
+    }
+    steps {
+        dsl {
+            text('''job ("\$DISPLAY_NAME") {
+                wrappers {
+                    preBuildCleanup()
+                }
+                steps {
+                  shell("make fclean")
+                  shell("make")
+                  shell("make test_run")
+                  shell("make clean")
+                }
+              }'''.stripIndent())
+        }
+    }
+}
