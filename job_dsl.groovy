ca960124cf8ac4eac402bd7b8bb34f3dea831b06
Noa <noa.trachez@epitech.eu>
2023-03-06 11:26:07 +0100

    [+] Adding my_marvin files

--- job_dsl.groovy
+++ job_dsl.groovy
@@ -1,4 +1,12 @@
-freeStyleJob('example') {
+folder('Tools') {
+    displayName('Tools')
+    description('Folder for miscellaneous tools.')
+}
+
+freeStyleJob('Tools/clone-repository') {
+    parameters {
+        stringParam('REPOSITORY_URL', ' ')
+        description('URL of the repository to clone.')
     steps {
         shell('echo Hello World!')
     }
